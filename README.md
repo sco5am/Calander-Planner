281e18e7c25a69ac0e9c1e16ac0d11ba6ccaa0ea
Sam Oaks <samuelclayoaks@yahoo.com>
2022-11-27 18:03:20 -0500

    added Readme txt

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # Calander-planer
+
+## Description
+
+Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+For this assigment i was tasked with creating a calander application that would save events for each hour of the work day. When an event is entered in the desired timeslot and the save button is clicked, then the appointment will be saved to the users local storage. The date that apprears at the top of the page, as well as the time tracking in the calander itself, are madde possible using the [Day.js](https://day.js.org/en/) library. Through this project i was able to better familerize myself with the jQuery API. 
+
+## Usage
+
+Provide instructions and examples for use. Include screenshots as needed.
+
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+
+    ```md
+    ![alt text](assets/images/screenshot.png)
+    ```
+
+
+## License
+
+MIT License
+
+Copyright (c) [2022] [SamuelOaks]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- assets/script.js
+++ assets/script.js
@@ -1,61 +1,52 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(document).ready(function () {
-// TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
   $('.saveBtn').click(function () {
 
     var value = $(this).siblings('.description').val();
-    var time = $(this).parent()('.time-block');
+    var time = $(this).parent()('time-block');
 
     localStorage.setItem(value, time);
 
     $('.feedback').addClass('show');
 
-    Alert(function () {
+   setTimeout(function () {
       $('.feedback').removeClass('show');
     }, 4000);
   });
   
     function updateHour() {
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
       var currentHour = dayjs().hour();
 
       $('.time-block').each(function() {
-      
-      
-      
-      
-      
-    })
+      var setHour =parseInt($(this).attr('id').split('-')[1]);
 
-
-     // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+      if (setHour === currentHour) {
+        $(this).addClass('present');
+      } else if (setHour < currentHour) {
+        $(this).removeClass('present');
+        $(this).addClass('past');
+      } else {
+        $(this).removeClass('past');
+        $(this).removeClass('present');
+        $(this).addClass('future');
+      }
+    });
+  }
   
-  });
+  updateHour();
+
+  setInterval(updateHour, 5000);
+
   
-  
 
-
-}
-
-
-
-
-
-
-
+  $('#hour-8 .description').val(localStorage.getItem('hour-8'));
+  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+  $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+  $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+  $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+  $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+  $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+  $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+  $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+  $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+  $('#today').text(dayjs().format('dddd, MMMM D, YYYY'));
+});
